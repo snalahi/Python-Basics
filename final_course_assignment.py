e7ff4b1dbea91f4d5c7d1564502f9b924ef60f18
Sk Nasimul Alahi <59950232+snalahi@users.noreply.github.com>
2021-05-13 07:14:48 -0700

    Update final_course_assignment.py

--- final_course_assignment.py
+++ final_course_assignment.py
@@ -22,3 +22,9 @@ acro = ""
 for w in org_lst:
     if w not in stopwords:
         acro = acro + (w[0].upper())
+
+
+# Write code that uses the string stored in sent and creates an acronym which is assigned to the variable acro. The first two letters of each word
+# should be used, each letter in the acronym should be a capital letter, and each element of the acronym should be separated by a “. ” (dot and space).
+# Words that should not be included in the acronym are stored in the list stopwords. For example, if sent was assigned the string “height and ewok
+# wonder” then the resulting acronym should be “HE. EW. WO”.
