8d88bb26a201dfd9e4a77e20ff98b1df38ed97cd
Sk Nasimul Alahi <59950232+snalahi@users.noreply.github.com>
2021-05-13 07:35:51 -0700

    Update final_course_assignment.py

--- final_course_assignment.py
+++ final_course_assignment.py
@@ -28,3 +28,23 @@ for w in org_lst:
 # should be used, each letter in the acronym should be a capital letter, and each element of the acronym should be separated by a “. ” (dot and space).
 # Words that should not be included in the acronym are stored in the list stopwords. For example, if sent was assigned the string “height and ewok
 # wonder” then the resulting acronym should be “HE. EW. WO”.
+
+stopwords = ['to', 'a', 'for', 'by', 'an', 'am', 'the', 'so', 'it', 'and', 'The']
+sent = "The water earth and air are vital"
+sent_lst = list(sent.split())
+acro = ""
+for w in sent_lst:
+    if w not in stopwords:
+        acro = acro + (w[0].upper()) + (w[1].upper())
+        if sent_lst.index(w) != len(sent_lst) - 1:
+            acro = acro + ". "
+
+            
+
+            
+            
+            
+            
+            
+            
+            
